c3a97db178cecba94b13e9de72912966681b500c
Alex Boylan <aboylan@users.noreply.github.com>
2024-06-15 16:03:59 -0600

    Codificando ejemplo ManyToOne con relacion existente

--- src/main/java/com/aboylan/curso/springboot/jpa/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
+++ src/main/java/com/aboylan/curso/springboot/jpa/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
@@ -1,5 +1,7 @@
 package com.aboylan.curso.springboot.jpa.springboot_jpa_relationship;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -25,7 +27,7 @@ public class SpringbootJpaRelationshipApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		manyToOne();
+		manyToOneFindByIdClient();
 	}
 
 	public void manyToOne() {
@@ -38,6 +40,22 @@ public class SpringbootJpaRelationshipApplication implements CommandLineRunner {
 		Invoice invoiceDB = invoiceRepository.save(invoice);
 
 		System.out.println(invoiceDB);
+	}
+
+	public void manyToOneFindByIdClient() {
+
+		Optional<Client> optionalClient = clientRepository.findById(1L);
+
+		if (optionalClient.isPresent()) {
+			Client client = optionalClient.orElseThrow();
+
+			Invoice invoice = new Invoice("Compras de oficina", 2000L);
+			invoice.setClient(client);
+			Invoice invoiceDB = invoiceRepository.save(invoice);
+
+			System.out.println(invoiceDB);
+		}
+
 	}
 
 }
